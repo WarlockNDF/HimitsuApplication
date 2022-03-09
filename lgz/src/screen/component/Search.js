bbbe03b9f6c8033c06fae94036160bbf43347565
Teammid <ja.natthakorn_st@tni.ac.th>
2022-03-09 17:28:26 +0700

    Search

--- lgz/src/screen/component/Search.js
+++ lgz/src/screen/component/Search.js
@@ -1,11 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet,View } from 'react-native'
 import React from 'react'
+import { VStack, Input, Button, IconButton, Icon, Text, NativeBaseProvider, Center, Box, Divider, Heading } from "native-base";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+
 
 const Search = () => {
     return (
-        <View style={styles.container}>
-            <Text>Search</Text>
-        </View>
+        <VStack style = {styles.container}>    
+      <VStack w="100%" space={5} alignSelf="center">
+        <Heading>Material</Heading>
+        <Input placeholder="Search"  InputLeftElement={<Icon m="2" ml="3" size="6" color="gray.400" as={<MaterialIcons name="search" />} />} />
+      </VStack>
+    </VStack>
     )
 }
 
@@ -15,7 +21,19 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
         justifyContent: 'center',
-    }
+    },
+    heading: {
+        fontSize: 35,
+        alignItems: "center",
+        fontWeight: "bold",
+    },
+      input: {
+        height: 40,
+        margin: 15,
+        borderWidth: 2,
+        borderRadius: 10,
+        backgroundColor: "#FDFEFE",
+        width: 375,
+      }
 })

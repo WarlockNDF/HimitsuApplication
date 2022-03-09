36bb2b840f3ac53a17ebe410c7716ffce5eb5a96
Lavy-Lallaby <77435549+Lavy-Lallaby@users.noreply.github.com>
2022-03-09 20:29:18 +0700

    dash

--- lgz/src/screen/component/DashBoard.js
+++ lgz/src/screen/component/DashBoard.js
@@ -10,216 +10,227 @@ import {
   VStack,
   Stack,
   NativeBaseProvider,
+  Pressable,
+  Spacer,
+  Flex,
+  Badge,
 } from "native-base";
 import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
-  AntDesign,
-  Feather,
+  Ionicons,
   MaterialCommunityIcons,
-  FontAwesome5,
+  AntDesign,
 } from "@expo/vector-icons";
 
 const DashBoard = () => {
   return (
-    <SafeAreaView
-      style={{ flex: 1, justifyContent: "center", alignContent: "center" }}
-    >
-      <View style={styles.container}>
-        <View style={styles.maginCard}>
-          <Box>
-            <Box
-              maxW="40"
-              rounded="lg"
-              overflow="hidden"
-              borderColor="coolGray.200"
-              borderWidth="1"
-              _dark={{
-                borderColor: "coolGray.600",
-                backgroundColor: "gray.700",
-              }}
-              _web={{
-                shadow: 2,
-                borderWidth: 0,
-              }}
-              _light={{
-                backgroundColor: "gray.50",
-              }}
-            >
-              <Box>
-                <AspectRatio w="100%" ratio={12 / 9}>
-                  <Center>
-                    <AntDesign name="clockcircleo" size={50} color="red" />
-                  </Center>
-                </AspectRatio>
-                <Center
-                  bg="violet.500"
-                  _dark={{
-                    bg: "violet.400",
-                  }}
-                  _text={{
-                    color: "warmGray.50",
-                    fontWeight: "700",
-                    fontSize: "xs",
-                  }}
-                  position="absolute"
-                  bottom="0"
-                  px="3"
-                  py="1.5"
-                >
-                  Nerly Expired
-                </Center>
-              </Box>
-            </Box>
-          </Box>
-        </View>
-        <View style={styles.maginCard}>
-          <Box>
-            <Box
-              maxW="40"
-              rounded="lg"
-              overflow="hidden"
-              borderColor="coolGray.200"
-              borderWidth="1"
-              _dark={{
-                borderColor: "coolGray.600",
-                backgroundColor: "gray.700",
-              }}
-              _web={{
-                shadow: 2,
-                borderWidth: 0,
-              }}
-              _light={{
-                backgroundColor: "gray.50",
-              }}
-            >
-              <Box>
-                <AspectRatio w="100%" ratio={12 / 9}>
-                  <Center>
-                    <MaterialCommunityIcons
-                      name="cart-arrow-down"
-                      size={50}
-                      color="black"
-                    />
-                  </Center>
-                </AspectRatio>
-                <Center
-                  bg="violet.500"
-                  _dark={{
-                    bg: "violet.400",
-                  }}
-                  _text={{
-                    color: "warmGray.50",
-                    fontWeight: "700",
-                    fontSize: "xs",
-                  }}
-                  position="absolute"
-                  bottom="0"
-                  px="3"
-                  py="1.5"
-                >
-                  Order
-                </Center>
-              </Box>
-            </Box>
-          </Box>
-        </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, margin: 4 }}>
+        <Text>DashBoardx</Text>
       </View>
+      <View
+        style={{
+          flex: 1,
+          margin: 4,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View style={{ flexDirection: "row" }}>
+          <Pressable>
+            {({ isHovered, isFocused, isPressed }) => {
+              return (
+                <Box
+                  width="40"
+                  borderWidth="1"
+                  borderColor="coolGray.300"
+                  shadow="3"
+                  bg={
+                    isPressed
+                      ? "coolGray.200"
+                      : isHovered
+                      ? "coolGray.200"
+                      : "coolGray.100"
+                  }
+                  p="5"
+                  m="2"
+                  rounded="8"
+                  style={{
+                    transform: [
+                      {
+                        scale: isPressed ? 0.96 : 1,
+                      },
+                    ],
+                  }}
+                >
+                  <VStack>
+                    <Center>
+                      <Text
+                        color="red.800"
+                        mt="3"
+                        fontWeight="medium"
+                        fontSize="xl"
+                        mb="3"
+                      >
+                        Nearly Expire
+                      </Text>
+                      <AntDesign name="clockcircle" size={35} color="red" />
+                    </Center>
+                  </VStack>
+                </Box>
+              );
+            }}
+          </Pressable>
 
-      <View style={styles.container}>
-        <View style={styles.maginCard}>
-          <Box>
-            <Box
-              maxW="40"
-              rounded="lg"
-              overflow="hidden"
-              borderColor="coolGray.200"
-              borderWidth="1"
-              _dark={{
-                borderColor: "coolGray.600",
-                backgroundColor: "gray.700",
-              }}
-              _web={{
-                shadow: 2,
-                borderWidth: 0,
-              }}
-              _light={{
-                backgroundColor: "gray.50",
-              }}
-            >
-              <Box>
-                <AspectRatio w="100%" ratio={12 / 9}>
-                  <Center>
-                    <Feather name="box" size={50} color="black" />
-                  </Center>
-                </AspectRatio>
-                <Center
-                  bg="violet.500"
-                  _dark={{
-                    bg: "violet.400",
+          <Pressable>
+            {({ isHovered, isFocused, isPressed }) => {
+              return (
+                <Box
+                  width="40"
+                  borderWidth="1"
+                  borderColor="coolGray.300"
+                  shadow="3"
+                  bg={
+                    isPressed
+                      ? "coolGray.200"
+                      : isHovered
+                      ? "coolGray.200"
+                      : "coolGray.100"
+                  }
+                  p="5"
+                  m="2"
+                  rounded="8"
+                  style={{
+                    transform: [
+                      {
+                        scale: isPressed ? 0.96 : 1,
+                      },
+                    ],
                   }}
-                  _text={{
-                    color: "warmGray.50",
-                    fontWeight: "700",
-                    fontSize: "xs",
-                  }}
-                  position="absolute"
-                  bottom="0"
-                  px="3"
-                  py="1.5"
                 >
-                  Low Stock
-                </Center>
-              </Box>
-            </Box>
-          </Box>
+                  <VStack>
+                    <Center>
+                      <Text
+                        color="coolGray.800"
+                        mt="3"
+                        fontWeight="medium"
+                        fontSize="xl"
+                        mb="3"
+                      >
+                        Order
+                      </Text>
+                      <MaterialCommunityIcons
+                        name="cart-arrow-down"
+                        size={35}
+                        color="black"
+                      />
+                    </Center>
+                  </VStack>
+                </Box>
+              );
+            }}
+          </Pressable>
         </View>
-        <View style={styles.maginCard}>
-          <Box>
-            <Box
-              maxW="40"
-              rounded="lg"
-              overflow="hidden"
-              borderColor="coolGray.200"
-              borderWidth="1"
-              _dark={{
-                borderColor: "coolGray.600",
-                backgroundColor: "gray.700",
-              }}
-              _web={{
-                shadow: 2,
-                borderWidth: 0,
-              }}
-              _light={{
-                backgroundColor: "gray.50",
-              }}
-            >
-              <Box>
-                <AspectRatio w="100%" ratio={12 / 9}>
-                  <Center>
-                    <FontAwesome5 name="boxes" size={50} color="black" />
-                  </Center>
-                </AspectRatio>
-                <Center
-                  bg="violet.500"
-                  _dark={{
-                    bg: "violet.400",
+
+        <View style={{ flexDirection: "row" }}>
+          <Pressable>
+            {({ isHovered, isFocused, isPressed }) => {
+              return (
+                <Box
+                  width="40"
+                  borderWidth="1"
+                  borderColor="coolGray.300"
+                  shadow="3"
+                  bg={
+                    isPressed
+                      ? "coolGray.200"
+                      : isHovered
+                      ? "coolGray.200"
+                      : "coolGray.100"
+                  }
+                  p="5"
+                  m="2"
+                  rounded="8"
+                  style={{
+                    transform: [
+                      {
+                        scale: isPressed ? 0.96 : 1,
+                      },
+                    ],
                   }}
-                  _text={{
-                    color: "warmGray.50",
-                    fontWeight: "700",
-                    fontSize: "xs",
-                  }}
-                  position="absolute"
-                  bottom="0"
-                  px="3"
-                  py="1.5"
                 >
-                  Stock
-                </Center>
-              </Box>
-            </Box>
-          </Box>
+                  <VStack>
+                    <Center>
+                      <Text
+                        color="yellow.800"
+                        mt="3"
+                        fontWeight="medium"
+                        fontSize="xl"
+                        mb="3"
+                      >
+                        Low Stock
+                      </Text>
+                      <MaterialCommunityIcons
+                        name="package-down"
+                        size={35}
+                        color="#FFDA02"
+                      />
+                    </Center>
+                  </VStack>
+                </Box>
+              );
+            }}
+          </Pressable>
+
+          <Pressable>
+            {({ isHovered, isFocused, isPressed }) => {
+              return (
+                <Box
+                  width="40"
+                  borderWidth="1"
+                  borderColor="coolGray.300"
+                  shadow="3"
+                  bg={
+                    isPressed
+                      ? "coolGray.200"
+                      : isHovered
+                      ? "coolGray.200"
+                      : "coolGray.100"
+                  }
+                  p="5"
+                  m="2"
+                  rounded="8"
+                  style={{
+                    transform: [
+                      {
+                        scale: isPressed ? 0.96 : 1,
+                      },
+                    ],
+                  }}
+                >
+                  <VStack>
+                    <Center>
+                      <Text
+                        color="coolGray.800"
+                        mt="3"
+                        fontWeight="medium"
+                        fontSize="xl"
+                        mb="3"
+                      >
+                        Stock
+                      </Text>
+                      <Ionicons
+                        name="file-tray-stacked-sharp"
+                        size={35}
+                        color="black"
+                      />
+                    </Center>
+                  </VStack>
+                </Box>
+              );
+            }}
+          </Pressable>
         </View>
       </View>
     </SafeAreaView>
@@ -230,16 +241,8 @@ export default DashBoard;
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
-    flexDirection: "row",
-  },
-  maginCard: {
-    marginLeft: 5,
-    marginRight: 5,
-    marginTop: 5,
-  },
-  flexBox: {
     flex: 1,
     justifyContent: "center",
+    flexDirection: "row",
   },
 });

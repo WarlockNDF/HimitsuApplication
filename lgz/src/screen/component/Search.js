5f09779a42cbc9f116056206cf9000e227329ed5
Teammid <ja.natthakorn_st@tni.ac.th>
2022-03-09 18:23:25 +0700

    mt 5

--- lgz/src/screen/component/Search.js
+++ lgz/src/screen/component/Search.js
@@ -8,7 +8,7 @@ const Search = () => {
   return (
     <SafeAreaView>
       <VStack>
-        <VStack mt={10} w="100%" space={5} alignSelf="center">
+        <VStack mt={7} w="100%" space={5} alignSelf="center">
           <Heading ml={10}>ค้นหาสินค้า</Heading>
           <View style={{paddingLeft:10,paddingRight:10}}>
             <Input placeholder="Search" InputRightElement={<Icon m="2" ml="3" size="6" color="gray.400" as={<MaterialIcons name="search" />} />} />

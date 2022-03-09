bba3908116b5806c7ad8c552d44af2ac666294bc
WarlockNDF <70856081+WarlockNDF@users.noreply.github.com>
2022-03-09 18:19:40 +0700

    Update: Search

--- lgz/src/screen/component/Search.js
+++ lgz/src/screen/component/Search.js
@@ -1,39 +1,43 @@
-import { StyleSheet,View } from 'react-native'
+import { StyleSheet, View, SafeAreaView } from 'react-native'
 import React from 'react'
 import { VStack, Input, Button, IconButton, Icon, Text, NativeBaseProvider, Center, Box, Divider, Heading } from "native-base";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
 
 const Search = () => {
-    return (
-        <VStack style = {styles.container}>    
-      <VStack w="100%" space={5} alignSelf="center">
-        <Heading>Material</Heading>
-        <Input placeholder="Search"  InputLeftElement={<Icon m="2" ml="3" size="6" color="gray.400" as={<MaterialIcons name="search" />} />} />
+  return (
+    <SafeAreaView>
+      <VStack>
+        <VStack mt={10} w="100%" space={5} alignSelf="center">
+          <Heading ml={10}>ค้นหาสินค้า</Heading>
+          <View style={{paddingLeft:10,paddingRight:10}}>
+            <Input placeholder="Search" InputRightElement={<Icon m="2" ml="3" size="6" color="gray.400" as={<MaterialIcons name="search" />} />} />
+          </View>
+        </VStack>
       </VStack>
-    </VStack>
-    )
+    </SafeAreaView>
+  )
 }
 
 export default Search
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        justifyContent: 'center',
-    },
-    heading: {
-        fontSize: 35,
-        alignItems: "center",
-        fontWeight: "bold",
-    },
-      input: {
-        height: 40,
-        margin: 15,
-        borderWidth: 2,
-        borderRadius: 10,
-        backgroundColor: "#FDFEFE",
-        width: 375,
-      }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 35,
+    alignItems: "center",
+    fontWeight: "bold",
+  },
+  input: {
+    height: 40,
+    margin: 15,
+    borderWidth: 2,
+    borderRadius: 10,
+    backgroundColor: "#FDFEFE",
+    width: 375,
+  }
 })

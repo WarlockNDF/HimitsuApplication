79d0219a0d32b4b176aef90cc19941d80e9e77c5
Dest1nes1a <77435539+Dest1nes1a@users.noreply.github.com>
2022-03-10 14:31:04 +0700

    Construc Nearly

--- lgz/src/screen/component/NearlyExpire.js
+++ lgz/src/screen/component/NearlyExpire.js
@@ -1,11 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, SafeAreaView, View } from 'react-native'
+import React, { useEffect, useState  } from 'react'
+import { VStack, Input, Button, IconButton, Icon, Text, NativeBaseProvider, Center, Box, Divider, Heading, Select, ScrollView } from "native-base";
+import http from "../../service/http";
+import StockCard from '../element/StockCard';
+
+
 
 const NearlyExpire = () => {
+
+  const [nearlyEx, setNearlyEx] = useState([]);
+
+  const getData = async () => {
+    try {
+      const { status, data } = await http.get('stock')
+      if (status !== 200) throw "No Such Product"
+      console.log(data.data);
+      setNearlyEx(data.data);
+    } catch (err) {
+      alert(err.message)
+      console.error(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+
   return (
-    <View>
-      <Text>NearlyExpire</Text>
-    </View>
+    <SafeAreaView>
+    <View style={{ alignItems: 'center' }}>
+            <ScrollView>
+              {
+                nearlyEx.map((nearlyExprie, index) => {
+                  const { StockID, product, Quantity} = nearlyExprie;
+                  return (
+                  <>
+                    <StockCard key={StockID+index} name={product.ProductName} typeName={product.productType.TypeName} numberOfStock={Quantity}/>
+                  </>
+
+                  )
+                })
+              }
+            </ScrollView>
+          </View>
+          </SafeAreaView>
   )
 }
 

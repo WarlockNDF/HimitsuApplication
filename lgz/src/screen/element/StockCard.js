d3bd67d229a9563e5eb71a89527ee12eb2ea7709
Lavy-Lallaby <77435549+Lavy-Lallaby@users.noreply.github.com>
2022-03-10 16:30:12 +0700

    stockcard

--- lgz/src/screen/element/StockCard.js
+++ lgz/src/screen/element/StockCard.js
@@ -1,39 +1,82 @@
 import React from "react";
-import { Pressable, Text, Box, HStack, Spacer, Flex, Center, NativeBaseProvider, Badge } from "native-base";
+import {
+  Pressable,
+  Text,
+  Box,
+  HStack,
+  Spacer,
+  Flex,
+  Center,
+  NativeBaseProvider,
+  Badge,
+} from "native-base";
 
 const StockCard = ({ name, typeName, numberOfStock }) => {
   return (
     <Pressable mt={15}>
-      {({
-        isHovered,
-        isFocused,
-        isPressed
-      }) => {
-        return <Box width={96} borderWidth="1" borderColor="coolGray.300" shadow="3" bg={isPressed ? "coolGray.200" : isHovered ? "coolGray.200" : "coolGray.100"} p="5" rounded="8" style={{
-          transform: [{
-            scale: isPressed ? 0.96 : 1
-          }]
-        }}>
-          <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
-            {`Name : `+name}
-          </Text>
-          <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xs">
-            {`Type : `+typeName}
-          </Text>
-          <Text mt="2" fontSize="sm" color="coolGray.700">
-            {`Quantity : `+numberOfStock}
-          </Text>
-          <Flex>
-            <Badge colorScheme={numberOfStock === 0? "danger" :"Blue"} _text={{
-              color: "white"
-            }} variant="solid" rounded="2">
-              {numberOfStock === 0 ? "OUT OF STOCK" : "Available"}
-            </Badge>
-          </Flex>
-        </Box>;
+      {({ isHovered, isFocused, isPressed }) => {
+        return (
+          <Box
+            width={96}
+            borderWidth="1"
+            borderColor="coolGray.300"
+            shadow="3"
+            bg={
+              isPressed
+                ? "coolGray.200"
+                : isHovered
+                ? "coolGray.200"
+                : "coolGray.100"
+            }
+            p="5"
+            rounded="8"
+            style={{
+              transform: [
+                {
+                  scale: isPressed ? 0.96 : 1,
+                },
+              ],
+            }}
+          >
+            <HStack>
+              <Text
+                color="coolGray.800"
+                mt="3"
+                fontWeight="bold"
+                fontSize="xl"
+              >
+                {`Name : ` + name}
+              </Text>
+              <Spacer />
+              <Text
+                color="darkBlue.800"
+                mt="4"
+                mr="2"
+                fontSize="md"
+              >
+                {`Quantity : ` + numberOfStock}
+              </Text>
+            </HStack>
+            <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xs">
+              {`Type : ` + typeName}
+            </Text>
+            <Flex>
+              <Badge
+                colorScheme={numberOfStock === 0 ? "danger" : "Blue"}
+                _text={{
+                  color: "white",
+                }}
+                variant="solid"
+                rounded="2"
+              >
+                {numberOfStock === 0 ? "OUT OF STOCK" : "Available"}
+              </Badge>
+            </Flex>
+          </Box>
+        );
       }}
     </Pressable>
-  )
-}
+  );
+};
 
-export default StockCard
+export default StockCard;

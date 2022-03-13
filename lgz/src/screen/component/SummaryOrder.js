abe8e3cd180210e8b9993abfa9011804ed0faaa9
Dest1nes1a <77435539+Dest1nes1a@users.noreply.github.com>
2022-03-13 23:26:49 +0700

    Update SummaryOrder.js

--- lgz/src/screen/component/SummaryOrder.js
+++ lgz/src/screen/component/SummaryOrder.js
@@ -1,4 +1,10 @@
-import { View, Text, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import React, { useState, useEffect } from "react";
 import { useCartContext } from "../../context/CartProvider";
 import http from "../../service/http";
@@ -9,47 +15,52 @@ const SummaryOrder = ({ navigation }) => {
   const { cart, cartAction } = useCartContext([]);
   const [products, setProducts] = useState([]);
   const productID = [];
+  const dataPost = [];
 
   const loopData = () => {
     cart.datas.forEach((context) => {
-      productID.push(context.productID)
-    })
+      productID.push(context.productID);
+    });
   };
 
   //GET ARRAY LIST PRODUCT by ID
   const getProduct = async () => {
     try {
-      let body = { ids: productID }
-      const { status, data } = await http.post('product/ids', body)
-      if (status !== 200) throw "No Such Product"
+      let body = { ids: productID };
+      const { status, data } = await http.post("product/ids", body);
+      if (status !== 200) throw "No Such Product";
       console.log(data.data);
       setProducts(data.data);
     } catch (err) {
-      alert(err.message)
+      alert(err.message);
       console.error(err.message);
     }
-  }
+  };
 
   const getQty = (id) => {
-    let idx = cart.datas.findIndex((cartProduct)=> cartProduct.productID === id)
-    if(idx === -1) return 
-    return cart.datas[idx].Quantity
-  }
+    let idx = cart.datas.findIndex(
+      (cartProduct) => cartProduct.productID === id
+    );
+    if (idx === -1) return;
+    return cart.datas[idx].Quantity;
+  };
 
   useEffect(async () => {
     loopData();
     getProduct();
-  }, [])
+  }, []);
 
-  // const createOrder = async () => {
-  //   await http.post("order", {Id: UserID}).then(async (res)=> {
-  //     consolse.log(res.data)
-  //   }).catch((err) => {
-  //     console.error(err);
-  //     alert("Can't Create Order!!")
-  //   })
-  // }
+  const test = [1001,20]
 
+  const createOrder = async () => {
+    await http.post("order", cart.datas).then(async (res)=> {
+      consolse.log(res.data)
+      console.log({productId: 1001, quantity: 10})
+    }).catch((err) => {
+      console.error(err);
+      alert("Can't Create Order!!")
+    })
+  }
 
   return (
     <SafeAreaView style={{ flex: 1, alignItems: "center" }}>
@@ -59,19 +70,50 @@ const SummaryOrder = ({ navigation }) => {
         </Text>
       </View>
       <View>
-        {
-          products.map((product) => {
-            return (
-              <>
-                <Text>{product.ProductName+" "+`${getQty(product.ProductID)}`}</Text>
-              </>
-            )
-          })
-        }
+        {products.map((product) => {
+          return (
+            <>
+              <Text>
+                {product.ProductName + " " + `${getQty(product.ProductID)}`}
+                {dataPost.push({productId: product.ProductID},{quantity: getQty(product.ProductID)})} 
+              </Text>
+            </>
+          );
+        })}
       </View>
 
+      <View>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => {
+           //alert(JSON.stringify(dataPost))
+           alert(JSON.stringify(cart.datas))
+           //createOrder()
+          }}
+        >
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>SUBMIT ORDER</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default SummaryOrder;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10,
+  },
+  buttonContainer: {
+    marginTop: 20,
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: "#f1c232",
+  },
+});
